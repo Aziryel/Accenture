1207fea23eb3b92c1ade1a65e224b202f5962ee8
Rafael Adrian <divinewarrioraziryel@gmail.com>
2022-03-07 12:53:46 -0600

    2

---
+++ PrimeraClase/src/test/java/ejemplos/Calculadora.java
@@ -0,0 +1,19 @@
+package ejemplos;
+
+public class Calculadora {
+
+    public static void main(String [] args)
+    {
+        //Declaracion de variables
+        int num1 = 10;
+        int num2 = 5;
+
+        //Operaciones
+
+        System.out.println("El resultado de la suma es: " +(num1 + num2));
+        System.out.println("El resultado de la resta es: " +(num1 - num2));
+        System.out.println("El resultado de la multiplicacion es: " +(num1 * num2));
+        System.out.println("El resultado de la division es: " +(num1 / num2));
+
+    }
+}
