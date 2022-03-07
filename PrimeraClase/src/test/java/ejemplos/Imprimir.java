405abdc88a12138d21324fddf623fe67785e441b
Rafael Adrian <divinewarrioraziryel@gmail.com>
2022-03-07 12:45:17 -0600

    1

---
+++ PrimeraClase/src/test/java/ejemplos/Imprimir.java
@@ -0,0 +1,12 @@
+package ejemplos;
+
+public class Imprimir {
+
+    public static void main(String [] args) {
+
+        String nombre = "Rafa!!";
+
+        System.out.println("Bienvenido " +nombre);
+
+    }
+}
